744900690c6c6edb890fd915a50bc23f3adf2427
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-08-26 02:01:17 +0530

    states
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 2-states/src/App.jsx
+++ 2-states/src/App.jsx
@@ -5,10 +5,41 @@ import './App.css';
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [name, setName] = useState('');
+	const [isVisible, setIsVisible] = useState(false);
+	// let counter = 0;
+	const increment = () => {
+		// counter++;
+		if (isVisible === false) {
+			setIsVisible(!isVisible);
+		}
+		setCount(count + 1);
+		console.log(`Counter Incremented ${count}`);
+	};
 
 	return (
 		<div className="App">
-			<h1>States</h1>
+			<div>
+				<h1>States</h1>
+				{isVisible ? (
+					<div>
+						<h2>Count:{count}</h2>
+					</div>
+				) : (
+					<div>
+						<h2>Not Visible</h2>
+					</div>
+				)}
+				<button onClick={increment}>Increment</button>
+			</div>
+			<div>
+				<input
+					type="text"
+					placeholder="Enter Name"
+				/>
+				<h3>{name}</h3>
+				<button value={name}>setName</button>
+			</div>
 		</div>
 	);
 }
